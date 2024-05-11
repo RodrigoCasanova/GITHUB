158e7297f5a49dca8925ce53bb5c6864fb39c373
Rodrigo <rodr.casanova@duocuc.cl>
2024-05-11 07:38:29 -0400

    arreglo de js y de los editar

--- mockup/js/adtienda.js
+++ mockup/js/adtienda.js
@@ -49,7 +49,11 @@ function mostrarFormularioEdicion(index) {
     document.getElementById("editPrecio").value = camiseta.precio.replace('$', ''); // Remover el símbolo de dólar
     document.getElementById("editIndex").value = index;
     document.getElementById("formularioEditar").style.display = "block";
+    
+    // Desplazarse hacia el formulario de edición
+    document.getElementById('formularioEditar').scrollIntoView({ behavior: 'smooth' });
 }
+
 
 function eliminarCamisetaDesdeFormulario() {
     let index = document.getElementById("editIndex").value;
@@ -90,184 +94,10 @@ function mostrarCamisetas() {
 window.onload = function () {
     mostrarCamisetas();
 };
-const ciudadesPorRegion = {
-    "Arica y Parinacota": ["Arica", "Putre"],
-    "Tarapacá": ["Iquique", "Alto Hospicio"],
-    "Antofagasta": ["Antofagasta", "Calama", "Tocopilla"],
-    "Atacama": ["Copiapó", "Vallenar", "Chañaral"],
-    "Coquimbo": ["La Serena", "Coquimbo", "Ovalle"],
-    "Valparaíso": ["Valparaíso", "Viña del Mar", "Quilpué"],
-    "Metropolitana de Santiago": ["Santiago", "Maipú", "Puente Alto"],
-    "Libertador General Bernardo O'Higgins": ["Rancagua", "Rengo", "San Fernando"],
-    "Maule": ["Talca", "Curicó", "Linares"],
-    "Ñuble": ["Chillán", "Chillán Viejo", "San Carlos"],
-    "Biobío": ["Concepción", "Talcahuano", "Chillán"],
-    "La Araucanía": ["Temuco", "Angol", "Victoria"],
-    "Los Ríos": ["Valdivia", "La Unión", "Río Bueno"],
-    "Los Lagos": ["Puerto Montt", "Osorno", "Puerto Varas"],
-    "Aysén del General Carlos Ibáñez del Campo": ["Coyhaique", "Puerto Aysén", "Chile Chico"],
-    "Magallanes y de la Antártica Chilena": ["Punta Arenas", "Puerto Natales", "Porvenir"]
-};
 
-function cargarCiudades() {
-    const regionSeleccionada = document.getElementById("region").value;
-    const ciudadDropdown = document.getElementById("ciudad");
-    ciudadDropdown.innerHTML = '<option value="">Selecciona una ciudad</option>';
-    const ciudades = ciudadesPorRegion[regionSeleccionada];
-    if (ciudades) {
-        ciudades.forEach(ciudad => {
-            const opcion = document.createElement("option");
-            opcion.text = ciudad;
-            opcion.value = ciudad;
-            ciudadDropdown.add(opcion);
-        });
-    }
-}
-
-function mostrarAlerta(region, ciudad, direccion) {
-    let alertaExistente = document.getElementById('alerta');
-    if (alertaExistente) {
-        alertaExistente.remove();
-    }
-    let alerta = document.createElement('div');
-    alerta.id = 'alerta';
-    alerta.classList.add('alerta');
-    alerta.innerHTML = `
-        <p>Tu pedido será enviado a la región de ${region}, ciudad de ${ciudad}, a la dirección: ${direccion}. El pedido estaria llegando en 5 días.</p>
-        <button class="boton" onclick="irAPagar()">Ir a Pagar</button>
-    `;
-    document.body.appendChild(alerta);
-}
-function validarEmail(input) {
-var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Expresión regular para validar correo electrónico
-if (!regex.test(input.value)) {
-    document.getElementById('email-error').innerText = "Por favor, introduce un correo electrónico válido.";
-    input.classList.add('error-input');
-} else {
-    document.getElementById('email-error').innerText = "";
-    input.classList.remove('error-input');
-}
-}
-
-document.getElementById('email').addEventListener('input', function() {
-validarEmail(this);
-});
-function validarTelefono(input) {
-    let regex = /^\d+$/; // Expresión regular que permite solo números
-    if (!regex.test(input.value)) {
-        input.value = input.value.replace(/[^\d]/g, ''); // Limpiar el campo si se ingresa algo que no es número
-    }
-}
-
-document.getElementById('telefono').addEventListener('input', function () {
-    validarTelefono(this); // Llamar a la función de validación cuando se ingrese algo en el campo de teléfono
-});
-
-
-document.getElementById('telefono').addEventListener('input', function () {
-    validarTelefono(this); // Llamar a la función de validación cuando se ingrese algo en el campo de teléfono
-});
-
-document.getElementById('formularioEnvio').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    let nombre = document.getElementById('nombre').value;
-    let email = document.getElementById('email').value;
-    let telefono = document.getElementById('telefono').value;
-    let region = document.getElementById('region').value;
-    let ciudad = document.getElementById('ciudad').value;
-    let direccion = document.getElementById('direccion').value;
-
-    if (nombre && email && telefono && region && ciudad && direccion) {
-        mostrarAlerta(region, ciudad, direccion);
-    } else {
-        alert('Por favor, completa todos los campos del formulario.');
+document.getElementById("precio").addEventListener("input", function(event) {
+    let input = event.target.value;
+    if (input.startsWith("-")) {
+        event.target.value = input.slice(1); // Eliminar el signo negativo
     }
 });
-
-
-function irAPagar() {
-    window.location.href = 'pago.html';
-}
-function cargarDatos() {
-         
-    let pedidoId = "1";
-    let usuario = "Usuario 1";
-    let productos = "Camiseta Colo Colo 2024, Camiseta Argentina Messi";
-    let direccion = "Concepcion, Paicavi 3280";
-
-  
-    document.getElementById("idCliente").value = pedidoId;
-    document.getElementById("nombreCliente").value = usuario;
-    document.getElementById("productos").value = productos;
-    document.getElementById("direccionCliente").value = direccion;
-}
-
-function generarFactura() {
-    // Obtener los valores de los campos
-    let idCliente = document.getElementById("idCliente").value.trim();
-    let nombreCliente = document.getElementById("nombreCliente").value.trim();
-    let direccionCliente = document.getElementById("direccionCliente").value.trim();
-    let productos = document.getElementById("productos").value.trim();
-
-    // Verificar que todos los campos estén completados
-    if (idCliente === "" || nombreCliente === "" || direccionCliente === "" || productos === "") {
-        alert("Por favor, complete todos los campos antes de generar la factura.");
-        return; // Detener la ejecución si algún campo está vacío
-    }
-
-    // Aquí puedes escribir el código para generar la factura y enviarla
-    // Por ejemplo, puedes enviar los datos a un servidor para procesar la factura
-    // y luego mostrar una confirmación al usuario
-
-    let factura = "Factura\n\n";
-    factura += "Número Id: " + idCliente + "\n";
-    factura += "Nombre Cliente: " + nombreCliente + "\n";
-    factura += "Dirección: " + direccionCliente + "\n";
-    factura += "Productos: " + productos;
-
-  
-    let ventanaFactura = window.open("", "_blank");
-    ventanaFactura.document.write("<pre>" + factura + "</pre>");
-}
-function validateForm() {
-    let cardHolderName = document.getElementById('card-holder-name').value;
-    let cardNumber = document.getElementById('card-number').value;
-    let expiryDate = document.getElementById('expiry-date').value;
-    let cvc = document.getElementById('cvc').value;
-    let address = document.getElementById('address').value;
-
-    let dateRegex = /^(0[1-9]|1[0-2])\/?([0-9]{2})$/;
-    let cardNumberRegex = /^\d+$/; // Expresión regular para permitir solo dígitos
-
-    // Validación para verificar si el número de tarjeta contiene letras
-    if (!cardNumber.match(cardNumberRegex)) {
-        document.getElementById('error-message').innerHTML = "El número de tarjeta solo puede contener dígitos.";
-        document.getElementById('success-message').innerHTML = "";
-        return false;
-    }
-
-    if (cardHolderName === "" || cardNumber === "" || expiryDate === "" || cvc === "" || address === "") {
-        document.getElementById('error-message').innerHTML = "Por favor, llene todos los campos.";
-        document.getElementById('success-message').innerHTML = ""; 
-        return false;
-    } else if (!expiryDate.match(dateRegex)) {
-        document.getElementById('error-message').innerHTML = "Por favor, ingrese una fecha de expiración válida (MM/YY).";
-        document.getElementById('success-message').innerHTML = "";
-        return false;
-    } else {
-        document.getElementById('success-message').innerHTML = "¡Pago exitoso!";
-        document.getElementById('error-message').innerHTML = ""; 
-        return true;
-    }
-}
-function validateKeyPress(event) {
-    let keyCode = event.keyCode || event.which;
-    let isValidKey = (keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122) || keyCode === 32;
-    if (!isValidKey) {
-        event.preventDefault();
-    }
-}
-
-
-
